48869f38842bcb799d0c760bb165565953e1017f
wlcharlie <wl00606352@gmail.com>
2021-04-29 02:05:28 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
-const port = 3000
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mondgodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.get('/', (req, res) => {
   res.send('XD')
 })
 
-app.listen(port, () => {
-  console.log(`WORKING! Running on http://localhost:${port}`)
+app.listen(3000, () => {
+  console.log(`WORKING! Running on http://localhost:3000`)
 })
