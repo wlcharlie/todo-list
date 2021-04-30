5c9527ddaddfc9d8fffd504cf8251b20882666de
wlcharlie <wl00606352@gmail.com>
2021-04-30 22:51:53 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -24,8 +24,6 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  console.log(Todo)
-
   Todo.find()
     .lean()
     .then(todos => res.render('index', { todos }))
@@ -48,6 +46,26 @@ app.get('/todos/:id', (req, res) => {
   return Todo.findById(id)
     .lean()
     .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
