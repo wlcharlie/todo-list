7de639b2280a58903f43d6bf615fa29b31a5d12f
wlcharlie <wl00606352@gmail.com>
2021-05-05 22:14:39 +0800

    refactor: modify mongoose config

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,16 +1,10 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo')
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mondgodb error!')
-})
 
 db.once('open', () => {
   console.log('mongodb connected')
+  console.log('running seeder...')
 
   for (let i = 0; i < 10; i++) {
     console.log(`creating ${i}`)
